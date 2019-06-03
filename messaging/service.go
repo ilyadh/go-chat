55b1faa2aba1f2cd8467612d27f0a508d91c3c54
Ilya Demukh <demukh.ilya@gmail.com>
2019-06-03 23:49:32 +0300

    Decouple messaging Service and Client

--- messaging/service.go
+++ messaging/service.go
@@ -2,71 +2,102 @@ package messaging
 
 import (
 	"context"
+	"fmt"
 	"log"
-
-	"github.com/gorilla/websocket"
+	"time"
 )
 
-// Service is a concurrent messaging service
-type Service struct {
-	clients    map[*client]bool
-	register   chan *client
-	unregister chan *client
-	messageBus chan *message
+// Client represents a messaging Service client
+type Client interface {
+	ID() string
+
+	Serve()
+	Send([]byte)
+
+	SetReceiveHandler(func(Client, []byte))
+	SetShutdownHandler(func(Client))
+
+	Shutdown(context.Context) error
 }
 
-// NewService creates new messaging services
+// Service manages Clients
+type Service struct {
+	clients    map[Client]bool
+	register   chan Client
+	unregister chan Client
+	broadcast  chan *payload
+}
+
+// NewService creates a new messaging Service
 func NewService() *Service {
 	return &Service{
-		clients:    make(map[*client]bool, 10),
-		register:   make(chan *client, 5),
-		unregister: make(chan *client, 5),
-		messageBus: make(chan *message, 5),
+		clients:    make(map[Client]bool),
+		register:   make(chan Client),
+		unregister: make(chan Client),
+		broadcast:  make(chan *payload),
 	}
 }
 
-// Run runs the messaging service
+// Run starts the messaging service
 func (s *Service) Run(ctx context.Context) {
-	log.Print("[DEBUG] Messaging service start")
-
 	for {
 		select {
-		case client := <-s.register:
-			log.Printf("[DEBUG] register client")
-			s.clients[client] = true
-		case client := <-s.unregister:
-			delete(s.clients, client)
-		case message := <-s.messageBus:
-			log.Printf("[DEBUG] Sending message %s to %d clients", string(message.msg), len(s.clients))
-			for client := range s.clients {
-				if client != message.client {
-					client.send(message.msg)
+		case c := <-s.register:
+			s.clients[c] = true
+			log.Printf("[INFO] Register Client: %s", c.ID())
+		case c := <-s.unregister:
+			delete(s.clients, c)
+			log.Printf("[INFO] Unregister Client: %s", c.ID())
+		case payload := <-s.broadcast:
+			for c := range s.clients {
+				if c != payload.client {
+					c.Send(payload.message)
 				}
 			}
 		case <-ctx.Done():
-			for client := range s.clients {
-				client.close()
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			done := make(chan error, len(s.clients))
+
+			for c := range s.clients {
+				go func(c Client) { done <- wrap(c.Shutdown(ctx), "[%s]", c.ID()) }(c)
 			}
+
+			for i := 0; i < cap(done); i++ {
+				if err := <-done; err != nil {
+					log.Printf("[ERROR] %v", err)
+				}
+			}
+			log.Printf("[WARN] Messaging service shutdown")
 			return
 		}
 	}
 }
 
-// RegisterAndServe creates a Client and registers it
-func (s *Service) RegisterAndServe(conn *websocket.Conn) {
-	client := newClient(s, conn)
+// Register adds Client to list of Clients
+func (s *Service) Register(client Client) {
+	client.SetReceiveHandler(func(c Client, message []byte) {
+		s.broadcast <- &payload{message: message, client: c}
+	})
+	client.SetShutdownHandler(func(c Client) {
+		s.unregister <- c
+	})
 
 	s.register <- client
-
-	client.serve()
 }
 
-func (s *Service) broadcast(msg []byte, client *client) {
-	log.Printf("[DEBUG] broadcast before %s", string(msg))
-	s.messageBus <- &message{msg: msg, client: client}
-	log.Printf("[DEBUG] broadcast after %s", string(msg))
+type payload struct {
+	message []byte
+	client  Client
 }
 
-func (s *Service) remove(client *client) {
-	s.unregister <- client
+func wrap(err error, format string, a ...interface{}) error {
+	if err == nil {
+		return err
+	}
+
+	a = append(a, err)
+
+	return fmt.Errorf(format+" %v", a...)
 }
